5b8901e1c8b7310d852d5b08c4f756419b6b8aca
Angie Vanegas P <arco527@gmail.com>
2024-04-30 15:09:54 +0000

    Almost done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net", ".us", ".io"];
+
+  let randomWords = array => {
+    return Math.floor(Math.random() * array.length);
+  };
+
+  let generator = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < ext.length; l++) {
+          const pronounRandom = randomWords(pronoun);
+          const adjRandom = randomWords(adj);
+          const nounRandom = randomWords(noun);
+          const extRandom = randomWords(ext);
+          generator.push([
+            pronoun[pronounRandom] +
+              adj[adjRandom] +
+              noun[nounRandom] +
+              ext[extRandom]
+          ]);
+        }
+      }
+    }
+  }
+  console.log(`${generator.length} combinaciones posibles`);
+  console.log(generator);
+
+  let element = document.getElementById("excuse");
+  element.innerHTML = excuse;
 };
